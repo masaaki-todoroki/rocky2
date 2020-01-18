0da0723cfa5c8f111a73e20410c43151408d5c9f
llllnen <llllnen@yahoo.co.jp>
2020-01-18 17:59:26 +0900

    カテゴリーによる投稿出し分け

--- lib/post-front/routes.php
+++ lib/post-front/routes.php
@@ -9,6 +9,6 @@ add_action( 'init', 'custom_rewrite_rule', 10, 0 );
 function get_news_archive_link( $event_type_slug = null, $news_type_slug = null ) {
 	$event_type_slug  = esc_html( $event_type_slug );
 	$news_type_slug  = esc_html( $news_type_slug );
-	$additional_path = ! is_null( $event_type_slug ) ? ( ! is_null( $news_type_slug ) ? $event_type_slug . '/' . $news_type_slug : $event_type_slug ) : '';
+	$additional_path = ! is_null( $event_type_slug ) ? ( ! is_null( $news_type_slug ) ? $event_type_slug . '/' . $news_type_slug . '/' : $event_type_slug . '/' ) : '';
 	return get_post_type_archive_link( 'news' ) . $additional_path;
 }
